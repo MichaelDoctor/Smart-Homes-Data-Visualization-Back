f11c0410c84ebe265a76a48928343080e4289090
Michael Doctor <michaelcdoctor@gmail.com>
2021-06-10 13:43:22 -0600

    Removed build files

--- build/controllers/utils/errorCreator.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// creates an error response
-const errorCreator = (statusCode, message, res) => res.status(statusCode).json({
-    type: 'ERROR',
-    statusCode,
-    message,
-});
-exports.default = errorCreator;
