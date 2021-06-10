ec14b6c831371438fa6d1e5cf99ab7dc7981e50f
Michael Doctor <michaelcdoctor@gmail.com>
2021-06-10 13:58:10 -0600

    fixed port

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ import crossOrigin from './middleware/crossOrigin';
 
 config();
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 // Check DB accessability
